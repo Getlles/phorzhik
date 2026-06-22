71312fa166344d7f4e3bee61413a9f67122e8458
Getlles <attencrow@gmail.com>
2026-06-23 00:34:05 +0300

    correct routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MainLayout } from "./layout/MainLayout/MainLayout";
 import { AccountPage } from "./pages/AccountPage/AccountPage";
+import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<MainLayout theme="login" />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+        
+        <Route element={<MainLayout theme="auth" />}>
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+
         <Route element={<MainLayout theme="account" />}>
-          <Route path="/" element={<Navigate to="/account" replace />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/account" element={<AccountPage />} />
         </Route>
       </Routes>
